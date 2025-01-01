038bdad0d79f6576157d87abc95e7fa919c0a9c9
vikasvermavikas <vikas.verma@prakharsoftwares.com>
2025-01-01 12:29:18 +0530

    skill changes

---
+++ app/Http/Controllers/hr/HrController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\hr;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HrController extends Controller
+{
+    /**
+     * Show HR Dashboard.
+    */
+    public function dashboard(Request $request){
+        return view(" hr.dashboard");
+    }
+}
