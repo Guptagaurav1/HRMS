412b2f6fc11e10f281dd5e9cbb983ae149f83934
vikasvermavikas <vikas.verma@prakharsoftwares.com>
2025-01-01 12:57:01 +0530

    skill changes

--- resources/views/hr/skill.blade.php
+++ resources/views/hr/skill.blade.php
@@ -1,83 +1,66 @@
 @extends('layouts.master')
 
 @section('style')
-<link rel="stylesheet" href="{{asset('assets/vendor/css/jquery-ui.min.css')}}"/>
-<link rel="stylesheet" href="{{asset('assets/vendor/css/select2.min.css')}}"/>
-
+<link rel="stylesheet" href="{{asset('assets/vendor/css/jquery-ui.min.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/vendor/css/select2.min.css')}}" />
 @endsection
 
-        
 @section('contents')
-    <div class="row">
-        <div class="col-12">
-            <div class="panel">
-                <div class="panel-header">
-                    <h5>Skill</h5>
+<div class="row">
+    <div class="col-12">
+        <div class="panel">
+            <div class="panel-header">
+                <h5>Skill</h5>
+            </div>
+            <div class="row my-4">
+                <div class="col-md-12 d-flex justify-content-start mx-3">
+                    <form class="row g-3">
+                        <div class="col-auto">
+                            <input type="text" class="form-control" placeholder="Enter Skill" required>
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-primary mb-3">Add Skills</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="row px-3 mt-2">
-                    <div class="col-md-3">
-                        <label class="form-label">Skills <span style="color: red">*</span></label>
-                            <input type="text" class="form-control form-control-sm">
-                       
-                        
 
-                    </div>
-                    <div class="col-md-3">
-                        {{-- <label class="form-label">Reporting Email</label>
-                        <select id="inputState" class="form-select">
-                            <option selected>Not Specify</option>
-                            <option>Select 1</option>
-                            <option>Select 1</option>
-                            <option>Select 1</option>
-                        </select>
 
-                        </label> --}}
-                        
-                    </div>
-                    <div class="col-md-6">
-                        <a href="{{'view-letter'}}"><button class="btn btn-sm btn-primary" style="margin-left: 120px;margin-top:25px">Add Skills</button></a>
-                        
-                    </div>
-                </div>
-                
-                <div class="panel-body">
-                   
-                    
-                    
+                <div class="table-responsive">
                     <table class="table table-bordered table-hover digi-dataTable all-employee-table table-striped" id="allEmployeeTable">
                         <thead>
                             <tr>
-                                
-                                
                                 <th>Sr No.</th>
                                 <th>Department</th>
                                 <th>Skills</th>
-                                
-                                
                                 <th>Action</th>
-                              
                             </tr>
                         </thead>
                         <tbody>
+                            @forelse($skills as $skill)
                             <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$skill->department}}</td>
+                                <td>{{$skill->skill}}</td>
                                 <td>
-                                    1
+                                    <a href="{{'view-letter'}}"><button class="btn btn-sm btn-primary">Delete</button></a>
                                 </td>
-                                <td>HR</td>
-                                <td>Tally , ERP9 , Excel</td>
-                             <td> 
-                               
-                                <a href="{{'view-letter'}}"><button class="btn btn-sm btn-primary">Delete</button></a>
-                            </td>
-                        </tr>
-                         
+                            </tr>
+                            @empty
+                            <tr>
+                                <td class="text-danger text-center" colspan="4">No Record Found</td>
+                            </tr>
+                            @endforelse
+
                         </tbody>
                     </table>
-                    <div class="table-bottom-control"></div>
+                    <div class="col-md-12 d-flex justify-content-center my-4">
+                        {{$skills->links() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 @endsection
