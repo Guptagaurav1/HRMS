7b919dbb017693a0e19621e71080abde117d3ba2
unknown <kusham90.com>
2025-03-28 11:28:09 +0530

    resolve some bug

--- routes/web.php
+++ routes/web.php
@@ -245,7 +245,6 @@ Route::middleware('auth')->prefix('hr')->group(function () {
     });
 
     /////////// workorder routes start ///////
-<<<<<<< HEAD
     Route::controller(WorkOrderController::class)->group(function (){
         Route::get("work-order-list","index")->name("work-order-list");
         Route::get("get-work-order","getWorkOrder")->name("get-work-order");
@@ -254,16 +253,6 @@ Route::middleware('auth')->prefix('hr')->group(function () {
         Route::get("edit-work-order/{id}","edit")->name("edit-work-order");
         Route::post("update-work-order/{id}","update")->name("update-work-order");
         Route::get("view-work-order/{id}","show")->name("view-work-order");
-=======
-    Route::controller(WorkOrderController::class)->group(function () {
-        Route::get("work-order-list", "index")->name("work-order-list");
-        Route::get("get-work-order", "getWorkOrder")->name("get-work-order");
-        Route::get("add-work-order", "create")->name("add-work-order");
-        Route::post("store-work-order", "store")->name("store-work-order");
-        Route::get("edit-work-order/{id}", "edit")->name("edit-work-order");
-        Route::post("update-work-order/{id}", "update")->name("update-work-order");
-        Route::get("view-work-order/{id}", "show")->name("view-work-order");
->>>>>>> 0e30d5d5d3c2344bf71f7b9297ba73d5008c8eb0
 
         Route::get("organisation-workOrder/{or_id}", "organisation_workOrder")->name("organisation-workOrder");
         Route::get("workOrder-details/{workOrder_id}", "workOrder_details")->name("workOrder-details");
