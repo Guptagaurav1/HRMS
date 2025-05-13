1fc8c4d9fa0d704e44bb5fe49849b786e4e1bb32
Gaurav Gupta <gaurav.gupta@prakharsoftwares.com>
2025-05-13 15:29:18 +0530

    Salary-reciept Ui FIx

--- resources/views/hr/leaves/leave-request-reciept.blade.php
+++ resources/views/hr/leaves/leave-request-reciept.blade.php
@@ -1,5 +1,50 @@
 @extends('layouts.master', ['title' => 'Leave Request Receipt'])
 
+@section('style')
+<style>
+@media print {
+    body, .panel, .printarea {
+        background: white !important;
+    }
+
+    .color-dark {
+        color: black !important;
+    }
+
+    .table-responsive {
+        overflow: visible !important;
+    }
+
+    .table th, .table td {
+        color: black !important;
+    }
+
+   
+label{
+    
+    color: black !important;
+    background-color: black;
+}
+
+    .btn, .d-flex.justify-content-end {
+        display: none !important;
+    }
+
+    @page {
+        size: A4 portrait;
+        margin: 10mm;
+    }
+
+    .printarea {
+        page-break-inside: avoid;
+    }
+
+}
+</style>
+@endsection
+
+
+
 
 @section('contents')
     <div class="fluid-container">
@@ -7,68 +52,69 @@
            
             <div class="col-md-12">
                 <div class="panel printarea">
-                    <div class="d-flex align-items-center justify-content-center">
+                    <div class="d-flex align-items-center justify-content-between px-4 py-3">
                         <img src="{{ asset('assets/images/PrakharNEWLogo.png') }}" alt="Logo" width="120"
                             class="text-center my-2">
+                            <img src="{{asset('assets\images\11years.png')}}" alt="Logo" style="width:90px">
                     </div>
 
                     <div class="invoice-header d-flex justify-content-center align-items-centerSS">
                         <div class="row justify-content-center w-100">
                             <div class="col-xl-7 col-lg-12 col-sm-12 text-center" id="invoice-center">
-                                <h3 class="prakhrar-heading">PRAKHAR SOFTWARE SOLUTIONS PVT. LTD</h3>
-                                <p>LGF, C-11, Malviya Nagar (Opp. SBI Bank), New Delhi - 110017</p>
-                                <p>Ph.No. 01140631622 &nbsp;&nbsp; Mail: hr@prakharsoftwares.com</p>
+                                <h3 class="prakhrar-heading color-dark">PRAKHAR SOFTWARE SOLUTIONS PVT. LTD</h3>
+                                <p class="color-dark">LGF, C-11, Malviya Nagar (Opp. SBI Bank), New Delhi - 110017</p>
+                                <p class="color-dark">Ph.No. 01140631622 &nbsp;&nbsp; Mail: hr@prakharsoftwares.com</p>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-md-12 table-responsive">
+                    <div class="col-md-12 table-responsive table-size">
                         <table class="table table-borderless table-hover digi-dataTable all-employee-table table-striped"
                             id="allEmployeeTable">
                             <tbody>
-                                <tr>
-                                    <th class="dark">
+                                <tr class="">
+                                    <th class="dark color-dark">
                                         Leave Code :
                                     </th>
-                                    <td>{{ $data->leave_code }}</td>
+                                    <td class="color-dark">{{ $data->leave_code }}</td>
                                     <th class="dark">
                                         Applied Date:
                                     </th>
-                                    <td>{{ date('M d, Y', strtotime($data->created_at)) }}</td>
+                                    <td class="color-dark">{{ date('M d, Y', strtotime($data->created_at)) }}</td>
                                 </tr>
                                 <tr>
-                                    <th class="dark">
+                                    <th class="dark color-dark">
                                         Name :
                                     </th>
-                                    <td>{{ $data->emp_name }}</td>
-                                    <th class="dark">
+                                    <td class="color-dark">{{ $data->emp_name }}</td>
+                                    <th class="dark color-dark">
                                         EMP Code :
                                     </th>
-                                    <td>{{ $data->emp_code }}</td>
+                                    <td class="color-dark">{{ $data->emp_code }}</td>
                                 </tr>
                                 <tr>
-                                    <th class="dark">
+                                    <th class="dark color-dark">
                                         Designation :
                                     </th>
-                                    <td>{{ $data->emp_designation }}</td>
-                                    <th class="dark">
+                                    <td class="color-dark">{{ $data->emp_designation }}</td>
+                                    <th class="dark color-dark">
                                         Department :
                                     </th>
-                                    <td>{{ $data->department }}</td>
+                                    <td class="color-dark">{{ $data->department }}</td>
                                 </tr>
                                 <tr>
-                                    <th class="dark">
+                                    <th class="dark color-dark">
                                         Type Of Leave Requested :
                                     </th>
                                     <td colspan="3">
-                                        <input class="form-check-input" type="checkbox"
+                                        <input class="form-check-input color-dark" type="checkbox"
                                             {{ $data->reason_for_absence == 'Sick leave' ? 'checked' : 'disabled' }}>
-                                        <label class="form-check-label" for="gridCheck">
+                                        <label class="form-check-label color-dark" for="gridCheck">
                                             Sick Leave
                                         </label>
-                                        <input class="form-check-input" type="checkbox"
+                                        <input class="form-check-input color-dark" type="checkbox"
                                             {{ $data->reason_for_absence == 'Casual leave' ? 'checked' : 'disabled' }}>
-                                        <label class="form-check-label" for="gridCheck">
+                                        <label class="form-check-label color-dark" for="gridCheck">
                                             Casual Leave
                                         </label>
 
@@ -143,13 +189,13 @@
                     </div>
                     <div class="row mt-2">
                         <div class="col-md-12 ">
-                            <p class="px-1"><span class="text-danger">Note : </span>You must submit request for absence,
+                            <p class="px-1 color-dark"><span class="text-danger">Note : </span>You must submit request for absence,
                                 other than sick leave, two days prior to
                                 the first day of your absent and please attach supportive document for sick leave</p>
                         </div>
                     </div>
                     <div class="panel-header">
-                        <h6>Approval/Disapproval Section</h6>
+                        <h6 class="color-dark">Approval/Disapproval Section</h6>
                     </div>
                     <div class="row mt-2 px-2">
                         <div class="col-md-6">
@@ -166,7 +212,7 @@
                         </div>
                     </div>
                     <div class="panel-header">
-                        <h6>Comment/Remarks</h6>
+                        <h6 class="color-dark">Comment/Remarks</h6>
                     </div>
                     <div class="row mt-2 px-2">
                         <div class="col-md-8 payment">
